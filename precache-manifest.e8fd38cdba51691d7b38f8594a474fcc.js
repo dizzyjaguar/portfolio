d570a883e2c98b1692847de9234562def089fd66
Eli Zevin <dizzyjaguar@gmail.com>
2020-05-23 11:51:02 -0700

    Updates

---
+++ precache-manifest.e8fd38cdba51691d7b38f8594a474fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e9faafb50c26b27cd97353cfb6ba00",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6be108f8615deecebc80",
+    "url": "/portfolio/static/css/main.ba9d8d09.chunk.css"
+  },
+  {
+    "revision": "db32ac34852c25de167e",
+    "url": "/portfolio/static/js/2.e6f3f3f5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.e6f3f3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be108f8615deecebc80",
+    "url": "/portfolio/static/js/main.18b3a12e.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  }
+]);
